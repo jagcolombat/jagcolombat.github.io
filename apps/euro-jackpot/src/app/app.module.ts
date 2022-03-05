b0a868ba2fa169b986232b201aa8b2dff4c9cb88
Jorge Antonio Gómez Colombat <jagcolombat@gmail.com>
2022-03-05 02:07:29 -0500

    add header and footer components

--- apps/euro-jackpot/src/app/app.module.ts
+++ apps/euro-jackpot/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EuroJackpotLayoutModule } from '@lottoland/euro-jackpot/layout';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, EuroJackpotLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/euro-jackpot/layout/src/lib/euro-jackpot-layout.module.ts
+++ libs/euro-jackpot/layout/src/lib/euro-jackpot-layout.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
-    MainLayoutComponent
+    MainLayoutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    MainLayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
 })
 export class EuroJackpotLayoutModule {}
