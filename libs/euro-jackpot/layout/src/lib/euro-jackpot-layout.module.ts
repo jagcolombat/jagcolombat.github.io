58e9387c50d1f40e7524fb8994ddb16701b189cd
Jorge Antonio Gómez Colombat <jagcolombat@gmail.com>
2022-03-05 00:45:46 -0500

    add main layout component

--- libs/euro-jackpot/layout/src/lib/euro-jackpot-layout.module.ts
+++ libs/euro-jackpot/layout/src/lib/euro-jackpot-layout.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    MainLayoutComponent
+  ],
 })
 export class EuroJackpotLayoutModule {}
