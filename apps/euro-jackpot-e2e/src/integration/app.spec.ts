af225eae9a32a4125756bbee5107b485ab709ff8
Jorge Antonio Gómez Colombat <jagcolombat@gmail.com>
2022-03-05 00:19:48 -0500

    create scaffolding and install dependencies

---
+++ apps/euro-jackpot-e2e/src/integration/app.spec.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('euro-jackpot', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome euro-jackpot');
+  });
+});
---
+++ apps/euro-jackpot/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lottoland-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'euro-jackpot';
+}
