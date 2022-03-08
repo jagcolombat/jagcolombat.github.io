6c1884f24dfb15f3da01cf1431034e4ae2a57ebb
Jorge Antonio Gómez Colombat <jagcolombat@gmail.com>
2022-03-08 12:44:38 -0500

    Add service results resolver for get data before show the page

--- libs/euro-jackpot/data-access/src/index.ts
+++ libs/euro-jackpot/data-access/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/euro-jackpot-data-access.module';
+export * from './lib/services/entities/eurojackpot-data-access.service'
---
+++ libs/euro-jackpot/results/src/lib/services/results-resolver.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { Resolve } from "@angular/router";
+import { EurojackpotDataAccessService } from "@lottoland/euro-jackpot/data-access";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class ResultsResolver implements Resolve<any> {
+
+    constructor(private euroDataServ: EurojackpotDataAccessService) {
+    }
+
+    resolve(): Observable<any> | Promise<any> | any {
+        return this.euroDataServ.getResults();
+    }
+
+}
