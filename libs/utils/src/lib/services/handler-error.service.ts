115f3d88b5d4718a395da70d80ad81b867781862
Jorge Antonio Gómez Colombat <jagcolombat@gmail.com>
2022-03-11 03:24:33 -0500

    Add error interceptor

---
+++ libs/utils/src/lib/services/handler-error.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HandlerErrorService {
+  error$: Subject<string> = new Subject();
+
+  sendErrorMsg(error: any) {
+    console.log('sendError', error);
+    let errorMessage = "Unknown error";
+    if(error.status === 400) {
+        errorMessage = "Error sending data";
+    } else if(error.status === 403) {
+        errorMessage = "User unauthorized";
+    } else if(error.status === 409) {
+        errorMessage = error.error.message;;
+    } else if(error.status >= 500) {
+        errorMessage = "Internal server error";
+    } else if(error.status >= 400 || error.status === 0) {
+        errorMessage = "Error communicating with server";
+    }
+    this.error$.next(errorMessage);
+  }
+}
